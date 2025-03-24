7f22f508592a81ff395879fdc3443ed3e905032b
DAYANA <est.daibarrab0201@0201.uml.edu-ni>
2025-03-23 21:30:35 -0600

    if-anidados-switch

--- if-anidados-switch.js
+++ if-anidados-switch.js
@@ -5,7 +5,7 @@ console.log ("El valor ingresado es" , monto)
 const numero = parseInt(monto)
 
 if(numero>500){
-console.log("has ingresado un valor>de500")
+console.log("has ingresado un valor>de500") 
 }else {}
     console.log("El numero es menor de 500")
 
